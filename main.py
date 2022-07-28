e7ac4003a9cf66c840050e7e9ebe2c70f21be2a4
SK <sarthakkarandikar03@gmail.com>
2022-07-28 12:40:54 +0530

    added exceptions and search func

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    new_data = {"website": website, "email": email, "password": password}
+    new_data = {website:{"email": email, "password": password}}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(
@@ -109,16 +109,19 @@ def save():
 
 
 def search():
+    website = website_entry.get()
     try:
-        with open("data.json", "r") as data_file:
+        with open("data.json") as data_file:
             data = json.load(data_file)
-            print(data)
-    except FileNotFoundError as e:
-        messagebox.showerror(title="Oops", message="File Not Found")
-    # else:
-    # website=website_entry.get()
-    # if website==data.get('website')
-
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -140,17 +143,17 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.grid(row=2, column=1,columnspan=2)
+email_entry.insert(0, "dummy@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-search_button = Button(text="Search", command=search)
+search_button = Button(text="Search", width=13, command=search)
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
